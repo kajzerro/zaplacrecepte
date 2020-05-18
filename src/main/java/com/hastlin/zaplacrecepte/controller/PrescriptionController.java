770fc604c49e4d1c68bca95730ce5f5dd26aa295
Jakub Osika <josika@visionapartments.com>
2020-05-18 14:41:21 +0200

    Api endpoint change

--- src/main/java/com/hastlin/zaplacrecepte/controller/PrescriptionController.java
+++ src/main/java/com/hastlin/zaplacrecepte/controller/PrescriptionController.java
@@ -2,17 +2,23 @@ package com.hastlin.zaplacrecepte.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mateuszkaszyk on 15/05/2020.
  */
 @RestController
+@RequestMapping("/api/prescriptions")
 public class PrescriptionController {
 
-    @GetMapping("/prescription")
+    @GetMapping("/")
     ResponseEntity createPrescription(){
-        return ResponseEntity.ok("BLABLA");
+        return ResponseEntity.ok("[\n" +
+                "  {\"name\": \"Piotrx\", \"surname\": \"Krzystyniak\", \"status\": \"paid\"},\n" +
+                "  {\"name\": \"Matuś\", \"surname\": \"Kajzer\", \"status\": \"unpaid\"},\n" +
+                "  {\"name\": \"Kubuś\", \"surname\": \"Osika\", \"status\": \"done\"}\n" +
+                "]");
     }
 
 }
