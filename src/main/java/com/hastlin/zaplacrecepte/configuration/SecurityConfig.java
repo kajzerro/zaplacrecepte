3a03c808f4ecea4b9fdf20ead13936f18b415b62
Jakub Osika <josika@visionapartments.com>
2020-05-19 16:42:56 +0200

    Remove default browser credentials popup

--- src/main/java/com/hastlin/zaplacrecepte/configuration/SecurityConfig.java
+++ src/main/java/com/hastlin/zaplacrecepte/configuration/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.hastlin.zaplacrecepte.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -29,6 +39,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .authenticationEntryPoint(authenticationEntryPoint());
     }
+
+    @Bean
+    public BasicAuthenticationEntryPoint authenticationEntryPoint() {
+        return new BasicAuthenticationEntryPoint() {
+            @Override
+            public void commence(
+                    HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+                    throws IOException {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                PrintWriter writer = response.getWriter();
+                writer.println("{\"message\":\""+authEx.getMessage()+"\"}");
+                log.error(authEx.getMessage());
+            }
+
+            @Override
+            public void afterPropertiesSet() {
+                setRealmName("zaplacrecepte.pl");
+                super.afterPropertiesSet();
+            }
+
+        };
+    }
+
+
 }
