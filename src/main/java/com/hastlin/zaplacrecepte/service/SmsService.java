f55fe4aaa400b115405f311b3b39ff6094924562
Jakub Osika <josika@visionapartments.com>
2020-08-11 21:50:12 +0200

    Sms extend logging

--- src/main/java/com/hastlin/zaplacrecepte/service/SmsService.java
+++ src/main/java/com/hastlin/zaplacrecepte/service/SmsService.java
@@ -3,13 +3,17 @@ package com.hastlin.zaplacrecepte.service;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-import com.amazonaws.services.sns.model.*;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class SmsService {
     public void sendSms(String message, String phoneNumber, String sender){
         AmazonSNS amazonSNS =  AmazonSNSClientBuilder.standard().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://sns.eu-central-1.amazonaws.com/", "eu-central-1")).build();
@@ -22,9 +26,10 @@ public class SmsService {
     }
     private void sendSMSMessage(AmazonSNS amazonSNS, String message,
                                       String phoneNumber, Map<String, MessageAttributeValue> smsAttributes) {
-         amazonSNS.publish(new PublishRequest()
+        PublishResult publishResult = amazonSNS.publish(new PublishRequest()
                 .withMessage(message)
                 .withPhoneNumber(phoneNumber)
                 .withMessageAttributes(smsAttributes));
+        log.info("SMS sent to number={} with message={} - result messageId={}", phoneNumber, message, publishResult.getMessageId());
     }
 }
